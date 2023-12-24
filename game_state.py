291871659eeaf81f1963bec0f04c2345e317bcdb
Veras-D <dveras2310@gmail.com>
2023-12-24 10:43:31 -0300

    Finish game_state.py

--- game_state.py
+++ game_state.py
@@ -1,6 +1,12 @@
 from enum import Enum
 
+
 class GameState(Enum):
-  NONE = 0,
-  RUNNING = 1,
-  ENDED = 2
+    NONE = 0,
+    RUNNING = 1,
+    ENDED = 2
+
+
+class CurrentGameState(Enum):
+    MAP = 0,
+    BATTLE = 1
